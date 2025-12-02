b11e77d92feee917c9bee8256e0bc91a423ca5e6
Ujwal Shrestha <chhyanggg119265@gmail.com>
2025-12-02 20:43:59 +0545

    Updated index, style and script file: Added feature of hamburger menu for mobile devices(media query)

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,71 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+
     var text = "Ujwal Shrestha";
     var typingElement = document.getElementById('typing-text');
     var index = 0;
 
     function typeText() {
         if (index < text.length) {
-            typingElement.innerText += text[index];
-            index++;
+            typingElement.innerText = typingElement.innerText + text[index];
+            index = index + 1;
             setTimeout(typeText, 120);
         }
     }
     setTimeout(typeText, 800);
 
-    var links = document.querySelectorAll('a[href^="#"]');
+    var links = document.getElementsByTagName('a');
     for (var i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', function (event) {
-            event.preventDefault();
-            var target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                window.scrollTo({
-                    top: target.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
+        var link = links[i];
+        if (link.getAttribute('href') && link.getAttribute('href').indexOf('#') === 0) {
+            link.onclick = function (event) {
+                event.preventDefault();
+                var targetId = this.getAttribute('href').substring(1);
+                var target = document.getElementById(targetId);
+                if (target) {
+                    var top = target.offsetTop;
+                    window.scrollTo(0, top);
+                }
+            };
+        }
     }
 
-    var revealElements = document.querySelectorAll('.reveal');
+    var revealElements = document.getElementsByClassName('reveal');
 
     function revealOnScroll() {
         for (var i = 0; i < revealElements.length; i++) {
             var element = revealElements[i];
             var elementPosition = element.getBoundingClientRect().top;
             var screenHeight = window.innerHeight;
-
             if (elementPosition < screenHeight - 100) {
-                element.classList.add('active');
+                element.className = element.className + " active";
             }
         }
     }
-    window.addEventListener('scroll', revealOnScroll);
+    window.onscroll = revealOnScroll;
     revealOnScroll();
 
-    var form = document.querySelector('form');
+    var form = document.getElementsByTagName('form')[0];
     if (form) {
-        form.addEventListener('submit', function (e) {
+        form.onsubmit = function (e) {
             e.preventDefault();
             alert("Thank you. Your message has been received.");
             form.reset();
-        });
+        };
+    }
+
+    var hamburger = document.getElementById('hamburger');
+    var navMenu = document.getElementsByTagName('nav')[0].getElementsByTagName('ul')[0];
+    if (hamburger) {
+        hamburger.onclick = function () {
+            if (navMenu.className.indexOf('active') === -1) {
+                navMenu.className = navMenu.className + " active";
+                hamburger.innerText = "✖";
+            } else {
+                navMenu.className = navMenu.className.replace(" active", "");
+                hamburger.innerText = "☰";
+            }
+            hamburger.blur();
+        };
     }
 
 });
